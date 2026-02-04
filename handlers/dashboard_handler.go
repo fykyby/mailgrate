f01e43e0c8ccf56603b154298b870344800e67c3
fykyby <95149002+fykyby@users.noreply.github.com>
2026-02-04 08:35:55 +0100

    Some UI tweaks

--- handlers/dashboard_handler.go
+++ handlers/dashboard_handler.go
@@ -2,12 +2,11 @@ package handlers
 
 import (
 	"app/helpers"
-	"app/templates/pages/app"
-	"net/http"
 
 	"github.com/labstack/echo/v5"
 )
 
 func DashboardShow(c *echo.Context) error {
-	return helpers.Render(c, http.StatusOK, app.Dashboard())
+	return helpers.Redirect(c, "/app/sync-lists")
+	// return helpers.Render(c, http.StatusOK, app.Dashboard())
 }
